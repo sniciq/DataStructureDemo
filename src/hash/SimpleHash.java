3fcd57d1978a1ae3ccd79cdc60458f187c46cd48
yanghanguang <yanghanguang@>
2015-08-27 17:53:34 +0800

    hash table

--- src/hash/SimpleHash.java
+++ src/hash/SimpleHash.java
@@ -1,5 +1,160 @@
 package hash;
 
+import java.util.Arrays;
+
 public class SimpleHash {
 
+	private int capacity;
+	private int size;
+	private Item[] values;
+	
+	public SimpleHash(int capacity) {
+		this.capacity = capacity;
+		values = new Item[capacity];
+		size = 0;
+	}
+	
+	public void put(String key, String value) {
+		int hashCode = getHashCode(key);
+		boolean isCover = false;
+		while(values[hashCode] != null) {
+			if(values[hashCode].getKey().equals(key)) {//已存在，替换，且size不加
+				isCover = true;
+				break;
+			}
+			hashCode = (hashCode + 1) % capacity;
+		}
+		values[hashCode] = new Item(key, value);
+		
+		if(!isCover) {
+			size ++;
+		}
+		
+		System.out.println(size + ", " + capacity);
+		if(size >= capacity) {//需要扩容
+			rehash();
+		}
+	}
+	
+	public String get(String key) {
+		int hashCode = getHashCode(key);
+		while(values[hashCode] != null && !values[hashCode].getKey().equals(key)) {
+			hashCode ++;
+		}
+		return values[hashCode].getValue();
+	}
+	
+	public void remove(String key) {
+		int hashCode = getHashCode(key);
+		while(values[hashCode] != null && !values[hashCode].getKey().equals(key)) {
+			hashCode ++;
+		}
+		
+		values[hashCode] = null;
+		size --;
+	}
+	
+	private int getHashCode(String key) {
+		return key.hashCode() % capacity;
+	}
+	
+	private void rehash() {
+		Item[] old = Arrays.copyOf(values, values.length);
+		this.capacity = this.capacity * 2 + 1;
+		this.values = new Item[this.capacity];
+		this.size = 0;
+		for(int i = 0; i < old.length; i++) {
+			if(old[i] != null) {
+				int hashCode = getHashCode(old[i].getKey());
+				boolean isCover = false;
+				while(values[hashCode] != null) {
+					if(values[hashCode].getKey().equals(old[i].getKey())) {
+						isCover = true;
+						break;
+					}
+					hashCode = (hashCode + 1) % capacity;
+				}
+				values[hashCode] = new Item(old[i].getKey(), old[i].getValue());
+				
+				if(!isCover) {
+					size ++;
+				}
+			}
+		}
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < values.length; i++) {
+			if(values[i] != null) {
+				sb.append(values[i]).append(",");
+			}
+		}
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		SimpleHash sh = new SimpleHash(2);
+		sh.put("1", "aaa");
+		sh.put("2", "b");
+		sh.put("3", "c");
+		sh.put("4", "d");
+		System.out.println(sh.toString());
+		System.out.println(sh.get("4"));
+
+		sh.put("4", "E");
+		System.out.println(sh.get("4"));
+		
+		sh.put("4", "G");
+		System.out.println(sh.get("4"));
+		
+		sh.remove("4");
+		System.out.println(sh.toString());
+		
+		sh.put("4", "d");
+		sh.put("5", "d");
+		sh.put("6", "d");
+		sh.put("7", "d");
+		sh.put("8", "d");
+		sh.put("9", "d");
+		sh.put("10", "d");
+		
+		System.out.println(sh.capacity);
+		System.out.println(sh.size);
+		System.out.println(sh.toString());
+	}
+}
+
+class Item {
+	private String key;
+	private String value;
+	
+	public Item(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public String toString() {
+		return "[" + key + "|" + value + "]";
+	}
 }
--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -5,14 +5,50 @@ import java.util.Arrays;
 public class MergeSort {
 	
 	public static void sort(int[] arr, int left, int right) {
+		if(left < right) {
+			int mid = left + (right - left) / 2;
+			sort(arr, left, mid);
+			sort(arr, mid + 1, right);
+			
+			mergePart(arr, left, mid, right);
+		}
+	}
+	
+	public static void mergePart(int[] arr, int left, int mid, int right) {
+		int[] tt = Arrays.copyOfRange(arr, left, mid);
+		System.out.print(Arrays.toString(tt) + "\t");
+		int[] ttt = Arrays.copyOfRange(arr, mid, right);
+		System.out.print(Arrays.toString(ttt) + "\t");
+		System.out.println();
 		
+		
+		int[] tempArr = Arrays.copyOf(arr, arr.length);
+		int i = left;
+		int j = mid + 1;
+		int k = left;
+		while(i <= mid && j <= right) {
+			if(tempArr[i] <= tempArr[j]) {
+				arr[k] = tempArr[i];
+				i ++;
+			}
+			else {
+				arr[k] = tempArr[j];
+				j ++;
+			}
+			k ++;
+		}
+		
+		while(i <= mid) {
+			arr[k] = tempArr[i];
+			k ++;
+			i ++;
+		}
 	}
 	
 	public static void main(String[] args) {
 		int[] arr = {11,22,4,54,6,8,12,56,7};
-		
-		
-		
+		System.out.println(Arrays.toString(arr));
+		MergeSort.sort(arr, 0, arr.length - 1);
 		System.out.println(Arrays.toString(arr));
 	}
 }
